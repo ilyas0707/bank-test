6cc5af3286d3032826c0093826a55cc075b626c1
ilyas0707 <48711796+ilyas0707@users.noreply.github.com>
2020-04-15 00:31:49 +0600

    apply

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 require("dotenv").config()
+const path = require("path")
 const mongoose = require("mongoose")
 
 const app = express()
@@ -8,9 +9,13 @@ app.use(express.json({ extended: true }))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 
-const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, "client/build")));
 
-//sadasd
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000
 
 async function start() {
     try {
