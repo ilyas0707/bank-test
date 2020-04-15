c6bdb4b3d01c1f88714f81ab3f1799f5fcc82ccc
ilyas0707 <48711796+ilyas0707@users.noreply.github.com>
2020-04-15 18:36:23 +0600

    debug

--- server.js
+++ server.js
@@ -13,14 +13,11 @@ app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(express.json({ extended: true }))
 app.use("/api/auth", require("./routes/auth.routes"))
+app.use(express.static('client/build'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
